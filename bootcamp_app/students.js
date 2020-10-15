c3526c46f9629ff0e333fe9a2ee60de71bc56c15
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-15 01:00:51 +0000

    parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,15 +8,16 @@ const pool = new Pool({
 });
 
 const processName = process.argv.slice(2);
-
-
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${processName[0]}%'
-LIMIT ${processName[1]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`; 
+const values = [`%${processName[0]}%`, processName[1]];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,14 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
-ORDER BY teacher;`)
+WHERE cohorts.name = $1
+ORDER BY teacher;`;
+
+const values = ['JUL02'];
+
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach( elementObj => {
     console.log(`${elementObj.cohort}: ${elementObj.teacher}`);
